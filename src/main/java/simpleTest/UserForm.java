c2d139b3d5433f6cc0f7095d4f6737ff5abb3def
simon <simon>
2019-07-25 18:05:54 +0800

    Servlet

--- src/main/java/simpleTest/UserForm.java
+++ src/main/java/simpleTest/UserForm.java
@@ -15,28 +15,35 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.apache.tomcat.util.buf.StringCache;
 
-public class DateServlet extends HttpServlet{
+public class UserForm extends HttpServlet{
 	// get request/respone object, overwrite service() in HttpServlet
 	public void service(HttpServletRequest request,HttpServletResponse response)
 	throws ServletException,IOException{
+		// get current date
 		Date date=new Date();
 		SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd");
-		String dateString=sdf.format(date);
+		String ds=sdf.format(date);
+		
+		request.setCharacterEncoding("utf-8");//for post only, before getParameter()
 		// get request data
-		String id=request.getParameter("id");
 		String name=request.getParameter("username");
+		String password=request.getParameter("password");
+		String email=request.getParameter("email");
 		Double h=Double.valueOf(request.getParameter("height"));
 		Double w=Double.valueOf(request.getParameter("weight"));
 		
-		// arrays of string
+		// arrays of string, no selection:return null
 		String[] subject=request.getParameterValues("subject");
-		for (String sbj : subject) {
-			System.out.println(sbj);
+		if(subject!=null) {
+			for (String sbj : subject) {
+				System.out.println(sbj);
+			}			
 		}
-		//response header, type of response, charset, default:iso8859-1, also by println
+		
+		//response header, type of response, charset, default:iso8859-1, also for println()
 		response.setContentType("text/html;charset=utf-8");	
-		response.getWriter().println("<h2>"+dateString+"</h2><h3>"+id+":"+name+
-				"</h3><h3>BMI: "+w/h/h+"</h3>");
+		response.getWriter().println("<h3>"+name+"-"+password+"</h3><h3>Email:"+email+
+				"</h3><h3>BMI: "+w/h/h+" "+ds+"</h3>");
 		//response.getWriter().close();//container close stream automatically
 	}
 
